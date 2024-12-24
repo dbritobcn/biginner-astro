444bee5bfa112c6c445626a941ee7681193d41ce
David Brito <david.brito.sge@mango.com>
2024-12-24 12:06:53 +0100

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.biginner.com",
   base: "/",
   trailingSlash: "always",
+  integrations: [
+    tailwind({
+      nesting: true,
+    }),
+  ],
 });
